104c69229f197a3232bb7a1902bded531b96b670
Leon Lee <leon.runlongli@gmail.com>
2025-12-01 17:15:19 +0800

    fix: await params

---
+++ components/ui/index.ts
@@ -0,0 +1,7 @@
+export * from "./button";
+export * from "./card";
+export * from "./input";
+export * from "./label";
+export * from "./textarea";
+export * from "./separator";
+export * from "./badge";
